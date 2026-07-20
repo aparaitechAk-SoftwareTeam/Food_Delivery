414f944f13d6b96aa59408c1757a8d76096a427f
aparaitechAk-SoftwareTeam <akansha.aparaitech@gmail.com>
2026-07-20 18:31:42 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,6 +2,6 @@
 const rawUrl = 
   import.meta.env.VITE_API_URL || 
   import.meta.env.VITE_API_BASE_URL || 
-  `https://food-delivery-ywd0.onrender.com/api`;
+  `https://food-delivery-fqqo.onrender.com/api`;
 
 export const API_BASE_URL = rawUrl.endsWith("/api") ? rawUrl : `${rawUrl}/api`;
